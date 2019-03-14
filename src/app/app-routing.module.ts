522fc6a1fda4b184b14f7aa0f47966446749c77e
miracle121 <ravilovmirahmad@gmil.com>
2019-03-14 18:45:25 +0500

    First commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserslistComponent } from './users/userslist/userslist.component';
 import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path:'', component : UserslistComponent},
   {path:'usercreate', component: UsersComponent },
-  {path:'edit/:userId',component:UsersComponent}
+  {path:'edit/:userId',component:UsersComponent},
+  {path:'login', component:LoginComponent}
  ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { FormsModule} from '@angular/forms';
-import{ ReactiveFormsModule} from '@angular/forms';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
-
+import { AppComponent} from './app.component';
+import { UsersComponent} from './users/users.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule} from '@angular/common/http';
 import {
   MatInputModule,
   MatCardModule,
@@ -17,22 +14,26 @@ import {
   MatExpansionModule,
   MatProgressSpinnerModule
 } from '@angular/material';
-import { from } from 'rxjs';
 import { HeaderComponent } from './header/header.component';
 import { UserslistComponent } from './users/userslist/userslist.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     HeaderComponent,
-    UserslistComponent
+    UserslistComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    MatInputModule,
+    FormsModule,
+     MatInputModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
